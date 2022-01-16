8df677d2c2a026aa76c3c2bd80bc6ae57edb8e30
IrinaTln <ikosenkova@gmail.com>
2022-01-16 11:32:25 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Петя", "Иванов", "LOG19_ag");
+            student1.generatRandomRating();
+
+
+            Student student2 = new Student("Таня", "Петрова", "TIT20_av");
+            student2.generatRandomRating();
+
+            Student student3 = new Student("Илон", "Маск", "TIG21_tv");
+            student3.generatRandomRating();
+
+           
+            Console.WriteLine(student1.getGroupeName() + " " + student1.getFirstName() + " " + student1.getLastName() +": " + student1.showRate());
+            Console.WriteLine(student2.getGroupeName() + " " + student2.getFirstName() + " " + student2.getLastName() +": " + student2.showRate());
+            Console.WriteLine(student3.getGroupeName() + " " + student3.getFirstName() + " " + student3.getLastName() +": " + student3.showRate());
+
+            checkRate(student1);
+            checkRate(student2);
+            checkRate(student3);
+
+            Console.ReadKey();
+        }
+
+        private static void checkRate(Student student)
+        {
+            if (student.validationRate() == true)
+            {
+                Console.WriteLine(student.getFirstName() + " " + student.getLastName() + " - включен в приказ на стипендию.");
+            }
+            else
+            {
+                Console.WriteLine(student.getFirstName() + " " + student.getLastName() + " - степению не получают.");
+            }
+        }
+        
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Student
+    {
+        private string FirstName;
+        private string LastName;
+        private string GroupeName;
+        private List<int> Rate;
+
+        public Student(string firstName, string lastName, string groupeName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.GroupeName = groupeName;
+            this.Rate = new List<int>();
+        }
+
+        public string getFirstName()
+        {
+            return this.FirstName;
+        }
+
+        public string getLastName()
+        {
+            return this.LastName;
+        }
+
+        public string getGroupeName()
+        {
+            return this.GroupeName;
+        }
+        public void addRate(int rate)
+        {
+            this.Rate.Add(rate);
+        }
+        
+        public bool validationRate()
+        {
+            foreach (int itemRate in this.Rate)
+            {
+                if (itemRate < 4)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void generatRandomRating()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                this.addRate(random.Next(2, 5));
+            }
+        }
+        
+        public string showRate()
+        {
+            string strDating = "";
+
+            foreach (int itemRate in this.Rate)
+            {
+                strDating += Convert.ToString(itemRate) + " ";
+            }
+
+            return strDating;
+        }
+    }
+}
